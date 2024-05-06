35cfed8899962249de3529546574cea024fdbadf
iMorejon <ismael.morejon@wenalyze.com>
2024-05-06 09:33:26 +0200

    chore create ui components

---
+++ src/app/message-box/message-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-box.component.html'
+})
+export class MessageBoxComponent {
+
+}
