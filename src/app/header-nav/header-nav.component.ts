8450c36800e0f07350fb97290a9c28d49561f2dd
iMorejon <ismael.morejon@wenalyze.com>
2024-05-06 11:18:16 +0200

    chore: create header navigation menu component

---
+++ src/app/header-nav/header-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-nav.component.html'
+})
+export class HeaderNavComponent {
+
+}
