c8f7ca248016ebc414e2f07d299aa671d7fdbb0b
iMorejon <ismael.morejon@wenalyze.com>
2024-05-06 09:49:33 +0200

    chore: create community panel component

---
+++ src/app/community-panel/community-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'community-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './community-panel.component.html'
+})
+export class CommunityPanelComponent {
+
+}
