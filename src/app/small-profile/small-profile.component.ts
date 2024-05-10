81c04c2d7b5e61974242351180c459136897ff46
iMorejon <ismael.morejon@wenalyze.com>
2024-05-10 11:04:50 +0200

    feat: add basic functionality to small profile component

--- src/app/small-profile/small-profile.component.ts
+++ src/app/small-profile/small-profile.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { CommunityPanelComponent } from '../community-panel/community-panel.component'
 
 @Component({
   selector: 'small-profile',
   standalone: true,
-  imports: [],
+  imports: [CommunityPanelComponent],
   templateUrl: './small-profile.component.html'
 })
 export class SmallProfileComponent {
 
+  smCommunityToggle () {
+    const toggleArrow = (button: Element) => {
+      let txtc = button.textContent
+      let arrow = txtc?.trim().slice(12)
+
+      if (arrow === '⬇️') arrow = '⬆️'
+      else arrow = '⬇️'
+
+      button.textContent = txtc?.trim().slice(0, 12) + arrow
+    }
+
+    const smallQuery = window.matchMedia("(width <= 768px)")
+    if (!smallQuery.matches) return
+
+    const $ = document.getElementById.bind(document)
+
+    const el = $('sm-dropdowm')
+    if (el) el.classList.toggle('hidden')
+
+    const button = $('toggler')
+    if (button) toggleArrow(button)
+  }
 }
